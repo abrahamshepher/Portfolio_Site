dfb37642fae1284b40950e399e06aa489bcd43c8
abrahamshepher <abrahamshepher@gmail.com>
2023-07-11 14:48:28 +0500

    changes with the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     <nav
       className={`${
         styles.paddingX
-      } rela w-full flex items-center py-5 fixed top-0 z-20 ${
+      } rela w-full flex items-center py-5 relative top-0 z-20 ${
         scrolled ? "bg-primary" : "bg-transparent"
       }`}
     >
